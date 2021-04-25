cef5e1f82764a160f60ee2f6ce96b424fb7d527e
vinay-vemuri <vinayvemuri31>
2021-04-25 15:11:44 -0400

    Log4j2 framework implementation

--- orderprocessing/src/main/java/com/ecommerce/orderprocessing/controller/OrdersController.java
+++ orderprocessing/src/main/java/com/ecommerce/orderprocessing/controller/OrdersController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.orderprocessing.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,14 @@ public class OrdersController {
 	@Autowired
 	public OrderprocessingService orderprocessingService;
 	
+	Logger log = LogManager.getLogger(OrdersController.class);
+	
 	@RequestMapping(value="/getOrderById/{id}",method=RequestMethod.GET, produces="application/json")
 	public @ResponseBody OrderDetails getOrderById(@PathVariable("id") String id) {
-		
+		log.info("info log");
+		log.warn("warn log");
+		log.error("error log");
+		log.debug("debug log");
 		return orderprocessingService.getOrderDetailsById(id);
 		
 	}
